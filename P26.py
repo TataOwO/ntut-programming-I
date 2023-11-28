4d90bc74bdc601b7dcbadab0264d66ec20663d14
TataOwO <tim0401.kao@gmail.com>
2023-11-28 13:25:04 +0800

    idk

---
+++ P26.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    population = int(input())
+    maxDay = int(input())
+    currentlyInfected = int(input())
+    potentialInfection = float(input())
+    recoverDays = int(input())
+    immunity = float(input())
